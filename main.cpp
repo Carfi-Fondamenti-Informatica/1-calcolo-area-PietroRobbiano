1453f392f546fc8b6774320f5823d015f332efb2
PietroRobbiano <100785610+PietroRobbiano@users.noreply.github.com>
2022-03-14 19:10:24 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 using namespace std;
-
 int main(){
-//Qui vanno definite le variabili
-
-cin >> a >> b >> c;
-
-//Qui va scritto il codice per eseguire i calcoli richiesti
-
-
+    float a=0;
+    float b=0;
+    float c=0;
+    cin >> a >> b >> c;
+    cout<< (a+b)/2 <<endl;
+    cout<< a*a <<endl;
+    cout<< a*b <<endl;
+    cout<< ((a+b)*c)/2 <<endl;
+    return 0;
 }
